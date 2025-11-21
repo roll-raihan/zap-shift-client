6ebb28580ba1f3701ad1ae1e371788df1ef2aa6d
roll-raihan <raihanahmednasim@gmail.com>
2025-11-21 23:33:22 +0600

    modified reviews section

--- src/pages/Home/Review/ReviewCard.jsx
+++ src/pages/Home/Review/ReviewCard.jsx
@@ -3,22 +3,19 @@ import quote from '../../../assets/reviewQuote.png'
 
 const ReviewCard = ({ review }) => {
 
-    const { userName, review: testimonial, user_photoURL } = review;
+    const { userName, review: testimonial, user_photoURL, user_email } = review;
 
     return (
         <div className="max-w-sm bg-white shadow-md rounded-2xl p-6 border border-gray-100">
             {/* Quote Icon */}
             <div className="text-teal-300 text-4xl mb-3"><img src={quote} alt="" /></div>
 
-
             {/* Review Text */}
-            <p className="text-gray-600 leading-relaxed mb-6">
+            <p className="mb-6">
                 {testimonial}
             </p>
 
-
             <div className="border-t border-dashed border-gray-300 my-4"></div>
-
 
             {/* Reviewer Info */}
             <div className="flex items-center gap-4 mt-4">
@@ -27,7 +24,7 @@ const ReviewCard = ({ review }) => {
                 </div>
                 <div>
                     <h3 className="font-semibold text-gray-800">{userName}</h3>
-                    <p className="text-sm text-gray-500">Senior Product Designer</p>
+                    <p className="text-sm text-gray-500">{user_email}</p>
                 </div>
             </div>
         </div>
--- src/pages/Home/Review/Reviews.jsx
+++ src/pages/Home/Review/Reviews.jsx
@@ -1,7 +1,7 @@
 import React, { use } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
-import { EffectCoverflow, Pagination } from 'swiper/modules';
+import { Autoplay, EffectCoverflow, Pagination } from 'swiper/modules';
 import ReviewCard from './ReviewCard';
 
 
@@ -11,25 +11,31 @@ const Reviews = ({ reviewPromise }) => {
     // console.log(reviews)
 
     return (
-        <div className='my-5'>
+        <div className='my-24'>
             <h2 className='font-bold text-3xl text-center mb-2'>What our customers are sayings</h2>
-            <p className='text-center text-wrap'>Enhance posture, mobility, and well-being effortlessly with Posture Pro. Achieve proper alignment, reduce pain, and strengthen your body with ease!</p>
+            <p className='text-center text-wrap my-6'>Enhance posture, mobility, and well-being effortlessly with Posture Pro. Achieve proper alignment, reduce pain, and strengthen your body with ease!</p>
             <div className=''>
                 <Swiper
+                    loop={true}
                     effect={'coverflow'}
                     grabCursor={true}
                     centeredSlides={true}
                     slidesPerView={4}
                     coverflowEffect={{
-                        rotate: 50,
-                        stretch: 0,
-                        depth: 100,
+                        rotate: 30,
+                        stretch: '50%',
+                        depth: 200,
                         modifier: 1,
+                        scale: 0.75,
                         slideShadows: true,
                     }}
+                    autoplay={{
+                        delay: 2000,
+                        disableOnInteraction: false,
+                    }}
                     pagination={true}
-                    modules={[EffectCoverflow, Pagination]}
-                    className="mySwiper"
+                    modules={[EffectCoverflow, Pagination, Autoplay]}
+                    className="mySwiper my-5"
                 >
                     {
                         reviews.map((review) => <SwiperSlide key={review.id}>
