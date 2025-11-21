6600fafdfb58d63b035da3a166e61e0afbf2ea2a
roll-raihan <raihanahmednasim@gmail.com>
2025-11-22 01:03:09 +0600

    search option on map is ongoing

--- src/pages/Coverage/Coverage.jsx
+++ src/pages/Coverage/Coverage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css'
 import { useLoaderData } from 'react-router';
@@ -7,16 +7,44 @@ const Coverage = () => {
 
     const position = [23.6850, 90.3563];
     const serviceCenters = useLoaderData();
+    const mapRef = useRef(null);
     // console.log(serviceCenters)
 
-    return (
-        <div className=''>
-            <h2 className="text-5xl text-center">We are available in 64 districts</h2>
-            <div>
+    const handleSearchDistrict = (e) => {
+        e.preventDefault();
+        const location = e.target.location.value;
+        const district = serviceCenters.find(c => c.district.toLowerCase().includes(location.toLowerCase()));
+        if (district) {
+            const coord = [district.latitude, district.longitude];
+            console.log(location,district)
+            mapRef.current.flyTo(coord, 14);
+        }
+    }
 
+    return (
+        <div className='my-10'>
+            <h2 className="text-4xl font-semibold text-center my-5">We are available in 64 districts</h2>
+            <div className='my-5 ml-5'>
+                <form onSubmit={handleSearchDistrict}>
+                    <label className="input">
+                        <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <g
+                                strokeLinejoin="round"
+                                strokeLinecap="round"
+                                strokeWidth="2.5"
+                                fill="none"
+                                stroke="currentColor"
+                            >
+                                <circle cx="11" cy="11" r="8"></circle>
+                                <path d="m21 21-4.3-4.3"></path>
+                            </g>
+                        </svg>
+                        <input type="search" required placeholder="Search" />
+                    </label>
+                </form>
             </div>
             <div className='border h-[800px] w-full'>
-                <MapContainer center={position} zoom={8} scrollWheelZoom={false} className='h-[800px]'>
+                <MapContainer center={position} zoom={8} scrollWheelZoom={false} ref={mapRef} className='h-[800px]'>
                     <TileLayer
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
